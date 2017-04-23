40fda433d3d0d93d89f68a03e9a88bd162ff8835
iGon <iGon@iGon-PC>
2017-04-23 22:11:02 +0900

    몽고 db 연결 완료_mybatis필요?
    하...개삽질했습니다요
    
    참고로 이거 디비 주소가 localhost라 참고해주세요

--- gongdaeobba/src/main/java/com/gongdaeobba/crawling/Crawling.java
+++ gongdaeobba/src/main/java/com/gongdaeobba/crawling/Crawling.java
@@ -11,12 +11,19 @@ public class Crawling {
 	public static void main(String[] args) {
 		
 
-		String articleURL = "http://shopping.naver.com/search/category.nhn?cat_id=50000089";   //신문기사 URL
+		String articleURL = "http://prod.danawa.com/info/?pcode=4576834&cate=112753";   //신문기사 URL
         Document doc;
         String str=null;
 		try {
 			doc = Jsoup.connect(articleURL).get();
-			Elements ele = doc.select("a.tit");          // 아이디가 _article인 div 태그 선택
+			Elements ele = doc.select("div");          // 아이디가 _article인 div 태그 선택
+			Object[] list = ele.toArray();
+			for(int i=0;i<list.length;i++){
+				
+				String t = list[i].toString();
+				String a = t.substring(t.indexOf("\">")+2, t.indexOf("</div>"));
+				System.out.println(a);
+			}
 			System.out.println(ele.text());
 			
 			//str = ele.select("div.goods_info_wrap").select("div.table_css_t2").select("div.table_cell").text();
